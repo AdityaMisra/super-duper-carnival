0669769dc1a991642b16646d4f84cd9017deaf79
Aditya Misra <amisra14@yahoo.co.uk>
2021-06-05 22:48:06 +0530

    [Testing] added unit test cases

--- test/test_file_io.py
+++ test/test_file_io.py
@@ -3,13 +3,20 @@ import pytest
 from util.file_io import FileIO
 from unittest.mock import patch, mock_open
 
+
 class TestFileIO:
 
-    def test_file_read(self):
+    def test_write(self):
         open_mock = mock_open()
-        with patch("main.open", open_mock, create=True):
-            FileIO.write("test-data")
+        with patch("util.file_io.open", open_mock, create=True):
+            FileIO.write("/tmp/output.txt", ["test-data"])
 
-        open_mock.assert_called_with("output.txt", "w")
+        open_mock.assert_called_with("/tmp/output.txt", "w")
         open_mock.return_value.write.assert_called_once_with("test-data")
-            # assert isinstance(FileIO.read(), list)
+
+    def test_read(self):
+        open_mock = mock_open()
+        with patch("util.file_io.open", open_mock, create=True):
+            data = FileIO.read("/tmp/output.txt")
+
+        open_mock.assert_called_with("/tmp/output.txt", "r")
