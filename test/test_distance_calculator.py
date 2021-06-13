7f2caae40ab697cfcdba94fa401f529c8b3d7ef1
Aditya Misra <amisra14@yahoo.co.uk>
2021-06-13 13:57:51 +0530

    Update test_distance_calculator.py

--- test/test_distance_calculator.py
+++ test/test_distance_calculator.py
@@ -5,6 +5,6 @@ from util.distance_calculator import DistanceCalculator
 
 class TestDistanceCalculator:
 
-    def test_gift_card_service(self):
+    def test_distance_calculator(self):
         dc = DistanceCalculator()
         assert int(dc.compute_distance((53.1229599, -6.2705202), (53.339428, -6.257664))) == 24
